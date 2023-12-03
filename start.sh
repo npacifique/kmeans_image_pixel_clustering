6b72dcda23b9a9ee746772e80f506d34edb00e62
Pacifique Ntahurunga <npacifique@pacsoft.io>
2023-12-03 16:16:57 -0500

    install python3 update rm

--- start.sh
+++ start.sh
@@ -6,7 +6,8 @@ docker_image_tag="${app}"
 # Check if the Docker image already exists
 if docker image inspect "${docker_image_tag}" &> /dev/null; then
     echo "Removing existing Docker image: ${docker_image_tag}"
-    docker image rm "${docker_image_tag} -f"
+    docker container rm "${docker_image_tag} -f"
+    docker image rm "${docker_image_tag}"
 fi
 
 # Build the Docker image
