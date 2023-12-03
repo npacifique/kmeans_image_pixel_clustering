4f5f69bbd5426ea45bd9cdd2298918f47c1d3315
Pacifique Ntahurunga <npacifique@pacsoft.io>
2023-12-03 16:27:06 -0500

    install python3 update rs

--- start.sh
+++ start.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 app="kmeans_image_extractor.api"
-docker_image_tag="${app}"
+docker_image_tag="${app}:latest"
+container="kmeans_extractor.api"
 
 # Check if the Docker image already exists
 if docker image inspect "${docker_image_tag}" &> /dev/null; then
     echo "Removing existing Docker image: ${docker_image_tag}"
-    docker container rm "${docker_image_tag} -f"
+    docker container rm "/${container} -f"
     docker image rm "${docker_image_tag} -f"
 fi
 
@@ -16,4 +17,4 @@ docker build -t "${app}" .
 # Run the Docker container
 docker run -d -p 8011:8011 \
   --name="${app}" \
-  -v "$PWD:/app" "${app}"
+  -v "$PWD:/app" "${container}"
