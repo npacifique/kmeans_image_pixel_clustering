5080cf0954e14eab835578ef78149a1773789bdd
Pacifique Ntahurunga <npacifique@pacsoft.io>
2023-12-03 16:44:34 -0500

    install python3 update rsxsssdf

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ container="kmeans_extractor.api"
 # Check if the Docker image already exists
 if docker image inspect "${docker_image_tag}" &> /dev/null; then
     echo "Removing existing Docker image: ${docker_image_tag}"
-    docker container rm "${container} -f"
-    docker image rm "${docker_image_tag} -f"
+    docker "container rm ${container} -f"
+    docker "image rm ${docker_image_tag} -f"
 fi
 
 # Build the Docker image
